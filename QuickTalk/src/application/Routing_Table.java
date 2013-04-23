1c524b644c85e6305e2a36b8458d82c88ac165bd
Tyler <tylerbtz@gmail.com>
2013-04-23 11:15:11 -0700

    added back in rons local changes

--- QuickTalk/src/application/Routing_Table.java
+++ QuickTalk/src/application/Routing_Table.java
@@ -1,7 +1,15 @@
 package application;
 
+import java.util.LinkedList;
+
 //To be completed by ron.  Methods must be static
 public class Routing_Table {
+	RClient[] clientlist;//this is a list of the clients and their addresses
+	
+	public Routing_Table(int start, int end){
+		int size = end-start+1;
+		clientlist = new RClient[120];
+	}
 
 	//needs to check a table to see if the to is on the socket number
 	public static boolean existOnThisSocket(int socket_number, String to) {
@@ -16,4 +24,15 @@ public class Routing_Table {
 
 
 
+}
+
+class RClient {
+	private int socket_number;
+	private String client_name;
+	 
+	public RClient(int thesocket_number, String theName){
+		socket_number = thesocket_number;
+		client_name = theName;
+	}
+	
 }
